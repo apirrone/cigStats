8fd4d690d8ab86cedb1ab580943b9e25e03c21d8
apirrone <antoine.pirrone@gmail.com>
2018-07-11 14:36:00 +0200

    more nice features

--- cigStats.cpp
+++ cigStats.cpp
@@ -1,22 +1,134 @@
 #include <iostream>
 #include <time.h>
 #include <iomanip>
+#include <stdio.h>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define CURRENCY "€"
 #define PRICE_OF_PACK 11
 #define PACKS_PER_WEEK 1
 
+typedef struct tm tm;
 
-time_t getDateInSeconds(){
-  
+bool checkDate(tm date){
+  int then = mktime(&date);
+  time_t now = time (NULL);
+
+  return (now-then)>0;
 }
 
-int main(){
 
+tm readFile(char* file){
+
+  tm date;
+    
+  if(access(file, F_OK)){// File does not exist  else
+    std::cout << "ERROR : file does not exist" << std::endl;
+    return date;
+  }
+  
+  std::string line;
+  std::ifstream myFile(file);
+  
+  if(myFile.is_open()){
+
+    getline(myFile, line);
+    date.tm_year = atoi(line.c_str());
+    
+    getline(myFile, line);
+    date.tm_mon = atoi(line.c_str());
+    
+    getline(myFile, line);
+    date.tm_mday = atoi(line.c_str());
+    
+    getline(myFile, line);
+    date.tm_hour = atoi(line.c_str());
+    
+    date.tm_min=0;
+    date.tm_sec=0;
+    
+    myFile.close();
+    return date;
+    
+  }
+  else{
+    std::cout << "ERROR : could not read file" << std::endl;
+    return date;
+  }
+}
+
+void writeFile(tm date, char* filename){
+
+  std::ofstream myFile(filename);
+  if(myFile.is_open()){
+    
+    myFile << date.tm_year << "\n";
+    myFile << date.tm_mon  << "\n";
+    myFile << date.tm_mday << "\n";
+    myFile << date.tm_hour << "\n";    
+    
+    myFile.close();
+    
+  }
+  else
+    std::cout << "ERROR : could not write file" << std::endl;  
+}
+
+
+
+time_t getDateInSeconds(tm date){
+  return mktime(&date);
+}
+
+void setup(char* file){
+
+  time_t time;
+  tm date;
+  int input;
+  bool dateOk = false;
+  
+  do{
+    
+    puts("Year ? : ");
+    scanf("%d", &input);
+    date.tm_year=input-1900;
+
+    puts("Month ? : ");
+    scanf("%d", &input);
+    date.tm_mon=input-1;
+
+    puts("Day ? : ");
+    scanf("%d", &input);
+    date.tm_mday=input;
+
+    puts("Hour ? : ");
+    scanf("%d", &input);
+    date.tm_hour=input;
+
+    date.tm_min=0;
+    date.tm_sec=0;
+
+    dateOk = checkDate(date);
+    
+    if(!dateOk)
+      std::cout << "Invalid date ! The date entered cannot be in the future" << std::endl;
+    
+  } while(!dateOk);
+    
+  writeFile(date, file);
+
+  std::cout << "Setup Done !" << std::endl;
+}
+
+void display(char* file){
   std::cout << std::fixed;
   std::cout << std::setprecision(2);
-
-  time_t start = 1527105520;
+  
+  tm date = readFile(file);  
+  int start = mktime(&date); // date in seconds elapsed since epoch
   time_t seconds = time (NULL);
   
   float hours = ((seconds - start)/60.)/60.;
@@ -73,7 +185,7 @@ int main(){
     std::cout << " hour ";
     
   
-  std::cout << "since last cigarette (2018 may 23 at 10PM)" << std::endl;
+  std::cout << "since last cigarette (2018 may 23 at 22h)" << std::endl;
 
   int hoursInWeek = 24*7;
   int pricePerWeek = PRICE_OF_PACK*PACKS_PER_WEEK;
@@ -83,7 +195,35 @@ int main(){
   std::cout << saved << CURRENCY << " saved " << std::endl;
 
   
-  std::cout << "----------------------------------------" << std::endl;
+  std::cout << "----------------------------------------" << std::endl;  
+}
+
+void usage(){
+  std::cout << "USAGE : ./cigStats" << std::endl;
+  std::cout << "OR" << std::endl;
+  std::cout << "USAGE : ./cigStats --setup" << std::endl;
+}
+
+int main(int argc, char* argv[]){
+  
+  char* file = (char*)".cigStatsConfig"; // to avoid annoying warning (depreciated conversion from string constant to char*)
+  
+  if(argc > 2){
+    usage();
+    return 0;
+  }
+
+  if(argc == 2){
+    if(strcmp(argv[1], "--setup") !=0){
+      usage();
+      return 0;
+    }
+    else
+      setup(file);
+  }
+
+  display(file);
+
   
   return 1;
 }
